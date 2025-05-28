467fc82a2c4a5c85ee0edc8f6c21e6c24f22c3d2
Daring Different <info@daringdifferent.com.au>
2025-05-28 23:12:06 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -166,7 +166,7 @@ const Navbar = () => {
                 transition={{ type: "spring", stiffness: 400, damping: 10 }}
               >
                 <img
-                  src="https://drive.google.com/thumbnail?id=1F5-60YjmLJzaJEMJYcl-A6BiQ4bU-oWh&sz=w1000"
+                  src="https://drive.google.com/thumbnail?id=1ckCamYVuXks6h2GXo081N6qiyTxHBemu&sz=w1000"
                   alt="Daring Different Logo"
                   className="h-12 w-12 object-contain"
                 />
@@ -293,7 +293,7 @@ const Navbar = () => {
                 whileTap={{ scale: 0.95 }}
               >
                 <img
-                  src="https://drive.google.com/thumbnail?id=1F5-60YjmLJzaJEMJYcl-A6BiQ4bU-oWh&sz=w1000"
+                  src="https://drive.google.com/thumbnail?id=1ckCamYVuXks6h2GXo081N6qiyTxHBemu&sz=w1000"
                   alt="Daring Different Logo"
                   className="h-10 w-10 object-contain"
                 />
