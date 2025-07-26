b6e1535b282c4312c5892f2a073369c53a864aab
innocent <paulinnocentojochenemi@gmail.com>
2025-07-26 16:46:47 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,89 @@
-// App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import ComingSoong from "./components/comingSoon.jsx";
 
-// Import auth provider and protected route
-import { AuthProvider } from './lib/AuthContext';
-import ProtectedRoute from './components/ProtectedRoutes';
+export default function App() {
+  return <ComingSoong />;
+}
 
-// Import components and pages
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Books from './pages/Books';
-import Podcast from './pages/Podcast';
-import Subscribe from './pages/Subscribe';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import UpdatePassword from './pages/UpdatePassword';
-import Profile from './pages/Profile';
-import AuthCallback from './pages/AuthCallback';
-import VideoPlayerPage from './pages/VideoPlayer';
-import Gallery from './pages/Gallery';
-import BookCiara from './pages/BookCiara';
-import Donate from './pages/Donate';
+// // App.tsx
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 
-const AppRoutes: React.FC = () => {
-  const location = useLocation();
-  // Extract backgroundLocation if set when navigating to a modal
-  const state = location.state as { backgroundLocation?: Location };
+// // Import auth provider and protected route
+// import { AuthProvider } from './lib/AuthContext';
+// import ProtectedRoute from './components/ProtectedRoutes';
 
-  return (
-    <>
-      <Navbar />
-      {/* Primary Routes */}
-      <Routes location={state?.backgroundLocation || location}>
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/podcast" element={<Podcast />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/book-ciara" element={<BookCiara />} />
-        <Route path="/donate" element={<Donate />} />
+// // Import components and pages
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Books from './pages/Books';
+// import Podcast from './pages/Podcast';
+// import Subscribe from './pages/Subscribe';
+// import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
+// import UpdatePassword from './pages/UpdatePassword';
+// import Profile from './pages/Profile';
+// import AuthCallback from './pages/AuthCallback';
+// import VideoPlayerPage from './pages/VideoPlayer';
+// import Gallery from './pages/Gallery';
+// import BookCiara from './pages/BookCiara';
+// import Donate from './pages/Donate';
 
-        {/* Auth routes */}
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
+// const AppRoutes: React.FC = () => {
+//   const location = useLocation();
+//   // Extract backgroundLocation if set when navigating to a modal
+//   const state = location.state as { backgroundLocation?: Location };
 
-        {/* Protected routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/subscribe" element={<Subscribe />} />
-          <Route path="/videos" element={<VideoPlayerPage />} />
-        </Route>
+//   return (
+//     <>
+//       <Navbar />
+//       {/* Primary Routes */}
+//       <Routes location={state?.backgroundLocation || location}>
+//         {/* Public routes */}
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/books" element={<Books />} />
+//         <Route path="/podcast" element={<Podcast />} />
+//         <Route path="/gallery" element={<Gallery />} />
+//         <Route path="/book-ciara" element={<BookCiara />} />
+//         <Route path="/donate" element={<Donate />} />
 
-        {/* Catch all route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+//         {/* Auth routes */}
+//         <Route path="/signin" element={<SignIn />} />
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/auth/callback" element={<AuthCallback />} />
 
-      {/* Modal overlay for /signup when backgroundLocation exists */}
-      {state?.backgroundLocation && (
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      )}
-    </>
-  );
-};
+//         {/* Protected routes */}
+//         <Route element={<ProtectedRoute />}>
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/update-password" element={<UpdatePassword />} />
+//           <Route path="/subscribe" element={<Subscribe />} />
+//           <Route path="/videos" element={<VideoPlayerPage />} />
+//         </Route>
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
-    </Router>
-  );
-};
+//         {/* Catch all route */}
+//         <Route path="*" element={<Navigate to="/" replace />} />
+//       </Routes>
 
-export default App;
+//       {/* Modal overlay for /signup when backgroundLocation exists */}
+//       {state?.backgroundLocation && (
+//         <Routes>
+//           <Route path="/signup" element={<SignUp />} />
+//           <Route path="/signin" element={<SignIn />} />
+//         </Routes>
+//       )}
+//     </>
+//   );
+// };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <AuthProvider>
+//         <AppRoutes />
+//       </AuthProvider>
+//     </Router>
+//   );
+// };
+
+// export default App;
